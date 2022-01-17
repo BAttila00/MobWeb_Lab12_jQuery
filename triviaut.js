b71bbc89fb3b9296e1614b1d2f593fdd3bfe9116
Attila Bruder <bruderattila@gmail.com>
2022-01-17 22:15:07 +0100

    A start-game-form-contents.html fájl tartalmát betöltjük az index "start-game-form" formjába.

--- triviaut.js
+++ triviaut.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+    $.get("start-game-form-contents.html").then(html => $("#start-game-form").html(html));
+
     $("#lets-play-button").on("click", function (e) {
         $(e.target).toggle();           //eltünteti azt az elemet amire kattintottunk -> "let's play" gombot
-        $("#start-game-form-section").toggle();     //megjeleníti a "loading..." feliratot
+        $("#start-game-form-section").toggle();     //megjeleníti a "loading..." feliratot (itt lesznek a játékbeállítások)
         $("#game-section").toggle();                //megjeleníti a játék section-t
     });
 })
