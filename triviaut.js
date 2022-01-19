d7ab83c6ca5f95741a5983ab2e5bfb230dd643e5
Attila Bruder <bruderattila@gmail.com>
2022-01-19 20:36:33 +0100

    Pár komment.

--- triviaut.js
+++ triviaut.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    $.get("start-game-form-contents.html").then(html => $("#start-game-form").html(html));
+    $.get("start-game-form-contents.html").then(html => $("#start-game-form").html(html));      //betölti a start-game-form-contents.html fájl tartalmát a játék paraméter választó formjába (start-game-form)
+    //ugyan az mint:
+    // $.get("start-game-form-contents.html").then(function(eredmeny) {
+    //     $("#start-game-form").html(eredmeny);
+    // });
 
     $("#lets-play-button").on("click", function (e) {
         $(e.target).toggle();           //eltünteti azt az elemet amire kattintottunk -> "let's play" gombot
