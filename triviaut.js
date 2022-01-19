fa7f86e6596527b128ef04f4c997c9bea50823af
Attila Bruder <bruderattila@gmail.com>
2022-01-19 21:30:22 +0100

    Menjen át a difficulty és a category is az API-ba.

--- triviaut.js
+++ triviaut.js
@@ -21,7 +21,12 @@ $(document).ready(function () {
         .on("submit", e => { // közvetlenül a HTML beszúrása után lácolhatjuk a 'submit' eseményre történő feliratkozást
             e.preventDefault(); // a böngésző alapértelmezett működését megállítjuk, amivel újratöltené az oldalt
             $("#start-game-form button[type='submit']").attr("disabled", true); // a Go! gombot letiltjuk, hogy ne lehessen újra API kérést indítani, amíg meg nem érkezett a válasz
-            $.get("https://opentdb.com/api.php?type=multiple&encode=base64&amount=" + $("[name='trivia_amount']").val()).then(data => {
+            var queryString = "";
+            queryString = "https://opentdb.com/api.php?type=multiple&encode=base64&amount=" + $("[name='trivia_amount']").val() +
+            "&category=" + $("[name='trivia_category'] :selected").val() + 
+            "&difficulty=" + $("[name='trivia_difficulty'] :selected").val();
+            console.log("queryString: " + queryString);
+            $.get(queryString).then(data => {
                 remainingQuestions = data.results;
                 console.log(remainingQuestions);
                 currentQuestion = 0;
